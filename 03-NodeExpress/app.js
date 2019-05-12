96e580a299c608ef51aef00a87cab19ec7606cc4
MedoDome <armin.comic@edu.fit.ba>
2019-05-12 13:15:57 +0200

    Added comments

--- 03-NodeExpress/app.js
+++ 03-NodeExpress/app.js
@@ -2,16 +2,19 @@
  * SETUP
  */
 
-var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser');
+var express = require('express'), // Express Server framework
+    app = express(), // We need to initialize our server
+    bodyParser = require('body-parser'); // Parsing middleware for BODY of the request
+    // https://www.npmjs.com/package/body-parser
 
-// support encoded bodies
+// Support encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// support json encoded bodies
+// Support json encoded bodies
 app.use(bodyParser.json());
 
+
+// Start our server
 app.listen(3500);
 
 
@@ -19,16 +22,55 @@ app.listen(3500);
  * ROUTES
  */
 
+ /**
+ * param(req) HTTP Request from client
+ * param(res) HTTP Response from server
+ */
+
 app.get('/', function (req, res) {
-    res.send("Welcome to Get West");
+
+    /**
+    * This is API request with VERB/METHOD GET 
+    * You can just type in browser http:localhost:3500 
+    * and you will get text Welcome to GET west
+    */
+
+    res.send("Welcome to GET West");
 });
+
+/**
+ * param(req) HTTP Request from client
+ * param(res) HTTP Response from server
+ */
 
 app.post('/', function (req, res) {
-    res.send("Welcome to Post West");
+    /**
+    * This is API request with VERB/METHOD POST 
+    * If you send POST request to http:localhost:3500 
+    *  you will get text Welcome to POST west
+    */
+    res.send("Welcome to POST West");
+
+    /**
+     * You can type in terminal curl -XPOST http://localhost:3500
+     * or you can use POSTMAN extension on your browser
+     * or any other tool
+     */
 });
 
+/**
+ * param(req) HTTP Request from client
+ * param(res) HTTP Response from server
+ */
+
 app.post('/1', (req, res) => {
-    res.send("Welcome to Post West 1");
+    res.send("Welcome to POST West 1");
+    /**
+    * This is API request with VERB/METHOD POST 
+    * If you send POST request to http:localhost:3500/1
+    *  you will get text Welcome to POST west
+    */
+
 });
 
 /**
@@ -40,11 +82,25 @@ const wanted = require("../02-NodeModuleExport/C-MultipleFunctionsExport/wantedM
 
 
 app.get('/wanted', function (req, res) {
+    
+    /**
+    * If you send GET request to http:localhost:3500/wanted
+    *  you will get all bandits from fake database
+    */
+
     const allBandits = wanted.getAllWantedBandits();
     res.send({ data: JSON.stringify(allBandits) });
 });
 
 app.post('/default', function (req, res) {
+
+    /**
+    * If you send POST request to http:localhost:3500/default
+    *  you will insert bandit with name Default
+    * 
+    * But here we called function addWantedBandits which require array of bandits
+    */
+
     const allBandits = wanted.addWantedBandits(["Default"]);
 
     res.send("OK !");
@@ -52,6 +108,15 @@ app.post('/default', function (req, res) {
 
 
 app.post('/add', function (req, res) {
+
+
+    /**
+    * If you send POST request to http:localhost:3500/add
+    * 
+    * We added bandit and then returned all bandits from our fake db
+    */
+
+
     // console.log(req);
     // res.send("OK");
 
@@ -72,6 +137,28 @@ app.post('/add', function (req, res) {
 
 
 app.get('/search', function (req, res) {
+
+
+
+    /**
+    * If you send get request to http:localhost:3500/search
+    * 
+    * To include name parameter in search we need to add query parameters
+    * to our request
+    * 
+    * GET method receive parameters inside url
+    * 
+    * http:localhost:3500/search?name=buffalo&lastname=bill
+    * 
+    * URI: http:localhost:3500/search
+    * Query string => name=buffalo&lastname=bill
+    * Query params: name , lastname
+    * 
+    * On end of url we need to add ? to start query parameters
+    * With & we add more query parameters
+    */
+
+
     // console.log(req);
     const { name } = req.query;
     // console.log(name);
@@ -86,6 +173,16 @@ app.get('/search', function (req, res) {
 
 app.patch('/wanted/:name', function (req, res) {
     // console.log(req);
+
+    /**
+    * If you send PATCH request to http:localhost:3500/wanted/buffalo
+    *  you will rename all bandits with name Buffalo inside fake db
+    * 
+    * It's possible to chain parameters too
+    * /wanted/:name/:lastname
+    * /wanted/:id
+    */
+
 
     const { name } = req.params;
 
